95b5bab5f2bb3f476d3cc017f74214db2fb5d9e2
Yichen Zhu <yzhu421@gatech.edu>
2017-03-02 01:55:06 -0800

    add feature for createplayer and managermode

---
+++ GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/AddPlayerActivity.java
@@ -0,0 +1,17 @@
+package edu.gatech.seclass.tourneymanager;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Owner on 2017/3/2.
+ */
+
+public class AddPlayerActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createplayer);
+    }
+
+}
---
+++ GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/InactivemanagermodeActivity.java
@@ -0,0 +1,33 @@
+package edu.gatech.seclass.tourneymanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+
+/**
+ * Created by Yichen Zhu on 2017/3/1.
+ */
+
+public class InactivemanagermodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inactivemanagermode);
+    }
+
+
+    public void addplayer(View view){
+        //TODO DB Implement
+
+        //end TODO
+        Toast.makeText(InactivemanagermodeActivity.this, "Add Player!", Toast.LENGTH_LONG).show();
+        startActivity(new Intent(InactivemanagermodeActivity.this, AddPlayerActivity.class));
+
+    }
+
+}
