25ee6c0bafe9928706ae90e099cd29cd00aa8842
Yichen Zhu <yzhu421@gatech.edu>
2017-03-03 10:25:12 -0800

    add listview feature for userlist and houseprofit list

--- GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/MainActivity.java
+++ GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/MainActivity.java
@@ -3,15 +3,8 @@ package edu.gatech.seclass.tourneymanager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-
-import java.util.List;
-
-import edu.gatech.seclass.tourneymanager.dao.PlayerDao;
-import edu.gatech.seclass.tourneymanager.dao.constants.Deck;
-import edu.gatech.seclass.tourneymanager.models.Player;
 
 public class MainActivity extends AppCompatActivity {
 
