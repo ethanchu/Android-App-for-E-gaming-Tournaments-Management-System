1c82433088fe5960af03a0f53168a83037877520
Yichen Zhu <yzhu421@gatech.edu>
2017-03-08 22:49:20 -0800

    add restrictions for username should be aphanumeric

--- GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/AddPlayerActivity.java
+++ GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/AddPlayerActivity.java
@@ -55,7 +55,7 @@ public class AddPlayerActivity extends AppCompatActivity {
             case "RAT": deck_db = Deck.RAT; break;
         }
 
-        if (Phone.getText().toString().matches("^[0-9]{10}$")) { // Check the phonenumber format
+        if (Phone.getText().toString().matches("^[0-9]{10}$") && Username.getText().toString().matches("[a-zA-Z 0-9]+")) { // Check the phonenumber format
             Integer playerID = DatabaseHelper.getInstance().getPlayerDao().createPlayer(Name.getText().toString(), Username.getText().toString(), Phone.getText().toString(), deck_db);
             if (playerID == -1) {
                 Toast.makeText(AddPlayerActivity.this, "Player has exist, was not created!", Toast.LENGTH_SHORT).show();
@@ -65,7 +65,7 @@ public class AddPlayerActivity extends AppCompatActivity {
             }
         }
         else {
-            Toast.makeText(AddPlayerActivity.this, "Phone number format is not correct(should be 10 digits)!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(AddPlayerActivity.this, "Phone number should be 10 digits and unique alphanumeric username!", Toast.LENGTH_SHORT).show();
         }
 
     }
